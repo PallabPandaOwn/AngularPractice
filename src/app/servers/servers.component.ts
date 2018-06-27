30264496bfbf7611e008209a090cf1596a73e65c
pallab.panda@heatsoftware.com <pallab.panda@heatsoftware.com>
2018-06-27 11:18:03 +0530

    Working on Bindings(String Interpolation and property binding and event Bindings

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,14 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  isEnable=false;
+  isEnable=false; // Initially disable
+  serverCreationStatus = "Server was not created"
+  serverName='';
   constructor() { 
+
+    //Disable after 2 sec
     setTimeout(()=>{
       this.isEnable=true;
     },2000)
+
+    //enable again after 10 sec
+    setTimeout(()=>{
+      alert("Enable Button Again !")
+      this.isEnable=true;
+    },5000)
+  }
+  ngOnInit() {
   }
 
-  ngOnInit() {
+  onServerCreation(){
+    this.serverCreationStatus="Server created";
+    this.isEnable=false
+  }
+
+  onServerUpdateName(event:Event){
+    this.serverName=(<HTMLInputElement>event.target).value;
   }
 
 }
